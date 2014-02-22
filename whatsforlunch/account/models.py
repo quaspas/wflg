e2e7a53fc6aa69058bf45282cb52aa753f67cab7
quaspas <daniel.quattro@gmail.com>
2014-02-22 15:13:12 -0500

    - extended AbstractBase user with User class. - extended BaseUserManger with UserManager class

--- whatsforlunch/account/models.py
+++ whatsforlunch/account/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_superuser(self, username, password, **extra_fields):
+        user = self.model(email=username, is_staff=True, is_active=True, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser):
+
+    class Meta:
+        db_table = 'user'
