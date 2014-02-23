a629f4dd0be2cf770b0f28b9a43eb238490c41a9
quaspas <daniel.quattro@gmail.com>
2014-02-23 16:19:09 -0500

    added home view get test

--- whatsforlunch/core/tests.py
+++ whatsforlunch/core/tests.py
@@ -4,13 +4,13 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
-
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class CoreViewTests(TestCase):
+
+    def test_home_get(self):
+        response = self.client.get(reverse('home'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
