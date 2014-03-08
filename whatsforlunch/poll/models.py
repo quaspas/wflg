3874984f5f22137c1ed1aadde5864624d8da8ac0
quaspas <daniel.quattro@gmail.com>
2014-03-07 21:25:21 -0500

    removed resteraunts from poll. going to put that in a seperate app

--- whatsforlunch/poll/models.py
+++ whatsforlunch/poll/models.py
@@ -4,7 +4,7 @@ from whatsforlunch.account.models import User
 
 class Poll(models.Model):
 
-    restaurants = models.ManyToManyField()
+    candidates = models.ManyToManyField()
     start       = models.DateTimeField()
     end         = models.DateTimeField()
     voters      = models.ManyToManyField()
@@ -13,23 +13,11 @@ class Poll(models.Model):
         db_table = 'poll'
 
 
-# you do not need an account to vote. only to make a poll.
-# so votes are cast by IP address? by cookie?
-
 class Vote(models.Model):
 
     user        = models.ForeignKey(User)
     poll        = models.ForeignKey(Poll)
-    choice      = models.ForeignKey(Restaurant)
+    choice      = models.ForeignKey()
 
     class Meta:
         db_table = 'vote'
-
-
-class Restaurant(models.Model):
-
-    name        = models.CharField()
-    location    = models.CharField()
-
-    class Meta:
-        db_table = 'restaurant'
